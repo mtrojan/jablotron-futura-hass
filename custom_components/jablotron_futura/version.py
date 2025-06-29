5e0aa7830ba1a420018b570f05023dc2e5dd0464
mtrojan <martintrojan@email.cz>
2025-06-30 00:12:53 +0200

    Create version.py

---
+++ custom_components/jablotron_futura/version.py
@@ -0,0 +1,7 @@
+# =============================================================================
+# File: version.py - Version information
+# =============================================================================
+
+"""Version information for Jablotron Futura integration."""
+
+VERSION = "1.0.0"
